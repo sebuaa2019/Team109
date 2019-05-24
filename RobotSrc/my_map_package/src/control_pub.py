b364398acd62ac70dd8b44abaf63d5561e9da5c0
jy0205 <15652579915@163.com>
2019-05-24 16:40:40 +0800

    fix movebase again

--- RobotSrc/my_map_package/src/control_pub.py
+++ RobotSrc/my_map_package/src/control_pub.py
@@ -6,58 +6,57 @@ from geometry_msgs.msg import Twist
 import sys, select, termios, tty
 import socket
  
-HOST = '127.0.0.1'  # 标准的回环地址 (localhost)
-PORT = 65432        # 监听的端口 (非系统级的端口: 大于 1023)
+HOST = '127.0.0.1'
+PORT = 65432
 
 
 def talker():
     '''control Publisher'''
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
-    s.listen()
+    s.listen(5)
+    conn, addr = s.accept()
 
-    with conn:
-        print "Control The Robot!"
-        print 'Connected by', addr
-        rospy.init_node('control_pub', anonymous=True)
-        vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
-        c = ''
-        now_x = 0
-        now_y = 0
-        now_z = 0
-        while not rospy.is_shutdown():
-            data = conn.recv(1024)
-            if not data:
-                c = ' '
-            else:
-                c = data.decode()
+    print 'Connected by', addr
+    rospy.init_node('control_pub', anonymous=True)
+    vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    c = ''
+    now_x = 0
+    now_y = 0
+    now_z = 0
+    while not rospy.is_shutdown():
+        data = conn.recv(1024)
+        if not data:
+            c = ' '
+        else:
+            c = data[0]
 
-            if c == 'w':
-                now_x = 0.1
-            if c == 'a':
-                now_y = 0.1
-            if c == 's':
-                now_x = -0.1
-            if c == 'd':
-                now_y = -0.1
-            if c == 'l':
-                now_z = 0.1
-            if c == 'r':
-                now_z = -0.1
-            if c == 't':
-                now_x = 0
-                now_y = 0
-                now_z = 0
+        if c == 'w':
+            now_x = 0.1
+        if c == 'a':
+            now_y = 0.1
+        if c == 's':
+            now_x = -0.1
+        if c == 'd':
+            now_y = -0.1
+        if c == 'l':
+            now_z = 0.1
+        if c == 'r':
+            now_z = -0.1
+        if c == 't':
+            now_x = 0
+            now_y = 0
+            now_z = 0
 
-            vel_cmd = Twist()
-            vel_cmd.linear.x = now_x
-            vel_cmd.linear.y = now_y
-            vel_cmd.linear.z = 0
-            vel_cmd.angular.x = 0
-            vel_cmd.angular.y = 0
-            vel_cmd.angular.z = now_z
-    
-            vel_pub.publish(vel_cmd)
+        vel_cmd = Twist()
+        vel_cmd.linear.x = now_x
+        vel_cmd.linear.y = now_y
+        vel_cmd.linear.z = 0
+        vel_cmd.angular.x = 0
+        vel_cmd.angular.y = 0
+        vel_cmd.angular.z = now_z
+
+        vel_pub.publish(vel_cmd)
 
 
 if __name__ == '__main__':
