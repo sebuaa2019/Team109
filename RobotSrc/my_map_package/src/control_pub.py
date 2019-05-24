265495e937ad9dd5ae1b7616482f69e5cac2d8bc
jy0205 <15652579915@163.com>
2019-05-24 15:49:01 +0800

    fix movebase again

--- RobotSrc/my_map_package/src/control_pub.py
+++ RobotSrc/my_map_package/src/control_pub.py
@@ -4,95 +4,64 @@
 import rospy
 from geometry_msgs.msg import Twist
 import sys, select, termios, tty
-
-
-def get_key_board():
-    tty.setraw(sys.stdin.fileno())
-    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
-    if rlist:
-        key = sys.stdin.read(1)
-    else:
-        key = ''
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-    return key
-
-
-def set_key():
-    linear_control = {'w':(1,0,0),'s':(-1,0,0),'a':(0,1,0),'d':(0,-1,0)}
-    angular_control = {'j':(0,0,1),'l':(0,0,-1)}
-    return linear_control,angular_control
+import socket
+ 
+HOST = '127.0.0.1'  # 标准的回环地址 (localhost)
+PORT = 65432        # 监听的端口 (非系统级的端口: 大于 1023)
 
 
 def talker():
     '''control Publisher'''
-    rospy.init_node('control_pub', anonymous=True)
-    vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
+    s.listen()
 
-    linear_control,angular_control = set_key()
-    linear_speed_x = 0.1
-    linear_speed_y = 0.1
-    angular_speed = 1
-    constant = 0
-    now_control = (0,0,0)
+    with conn:
+        print "Control The Robot!"
+        print 'Connected by', addr
+        rospy.init_node('control_pub', anonymous=True)
+        vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+        c = ''
+        now_x = 0
+        now_y = 0
+        now_z = 0
+        while not rospy.is_shutdown():
+            data = conn.recv(1024)
+            if not data:
+                c = ' '
+            else:
+                c = data.decode()
 
-    print "Control The Robot!"
+            if c == 'w':
+                now_x = 0.1
+            if c == 'a':
+                now_y = 0.1
+            if c == 's':
+                now_x = -0.1
+            if c == 'd':
+                now_y = -0.1
+            if c == 'l':
+                now_z = 0.1
+            if c == 'r':
+                now_z = -0.1
+            if c == 't':
+                now_x = 0
+                now_y = 0
+                now_z = 0
+
+            vel_cmd = Twist()
+            vel_cmd.linear.x = now_x
+            vel_cmd.linear.y = now_y
+            vel_cmd.linear.z = 0
+            vel_cmd.angular.x = 0
+            vel_cmd.angular.y = 0
+            vel_cmd.angular.z = now_z
     
-    while not rospy.is_shutdown():
-        ins = get_key_board()
-        if ins in linear_control.keys():
-            now_control = linear_control[ins]
-            constant = 0
+            vel_pub.publish(vel_cmd)
 
-        elif ins in angular_control.keys():
-            now_control = angular_control[ins]
-            constant = 0
-
-        elif ins == '1':
-            if linear_speed_x < 0.5:
-                linear_speed_x += 0.05
-            if linear_speed_y < 0.5:
-                linear_speed_y += 0.05
-
-        elif ins == '2':
-            if linear_speed_x > 0.1:
-                linear_speed_x -= 0.1
-            if linear_speed_y > 0.1:
-                linear_speed_y -= 0.1
-
-        elif ins == ' ':
-            now_control = (0,0,0)
-        
-        else:
-            constant += 1
-            if constant > 5:
-                now_control = (0,0,0)
-
-        now_speed_x = linear_speed_x * now_control[0]
-        now_speed_y = linear_speed_y * now_control[1]
-        now_angular_speed = angular_speed * now_control[2]
-
-
-        vel_cmd = Twist()
-        vel_cmd.linear.x = now_speed_x
-        vel_cmd.linear.y = now_speed_y
-        vel_cmd.linear.z = 0
-        vel_cmd.angular.x = 0
-        vel_cmd.angular.y = 0
-        vel_cmd.angular.z = now_angular_speed
-        vel_pub.publish(vel_cmd)
-
-
-    # rate = rospy.Rate(10) # 10hz
-    # while not rospy.is_shutdown():
-    #     hello_str = "hello world %s" % rospy.get_time()
-    #     rospy.loginfo(hello_str)
-    #     pub.publish(hello_str)
-    #     rate.sleep()
 
 if __name__ == '__main__':
-    settings = termios.tcgetattr(sys.stdin)
     try:
         talker()
     except rospy.ROSInterruptException:
         pass
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
--- RobotSrc/myweb/apps/movebase/views.py
+++ RobotSrc/myweb/apps/movebase/views.py
@@ -2,21 +2,30 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
 import json
 import os
+import socket
 
 # Create your views here.
 
+HOST = '127.0.0.1'  # 服务器的主机名或者 IP 地址
+PORT = 65432        # 服务器使用的端口
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+# def write_file(content):
+#     home_path = os.popen('echo $HOME').readlines()[0].strip()
+#     path = home_path + '/ros_test/src/my_control/src/keys'
+#     fw = open(path,'a')
+#     fw.write(content)
 
 def write_file(content):
-    home_path = os.popen('echo $HOME').readlines()[0].strip()
-    path = home_path + '/ros_test/src/my_control/src/keys'
-    fw = open(path,'a')
-    fw.write(content)
+    s.sendall(content.encode())
 
 
 def move_map():
     home_path = os.popen('echo $HOME').readlines()[0].strip()
     rootdir = home_path
-    goaldir = home_path + '/catkin_ws/src/wpb_home/wpb_home_tutorials/maps'
+    goaldir = home_path + '/catkin_ws/src/team_109/clean_moudle/config'
     os.system('mv ' + rootdir + '/map.pgm' + ' ' + goaldir + '/map.pgm')
     os.system('mv ' + rootdir + '/map.ymal' + ' ' + goaldir + '/map.ymal')
 
@@ -27,19 +36,19 @@ def move(request):
     json_data = json.loads(request.POST['json_data'])
     type = json_data['type']
     if type == 'Turn Left':
-        write_file('l\n')
+        write_file('l')
     elif type == 'Forwards':
-        write_file('w\n')
+        write_file('w')
     elif type == 'Turn Right':
-        write_file('r\n')
+        write_file('r')
     elif type == 'Move Left':
-        write_file('a\n')
+        write_file('a')
     elif type == 'Backward':
-        write_file('s\n')
+        write_file('s')
     elif type == 'Move Right':
-        write_file('d\n')
+        write_file('d')
     elif type == 'Begin':
-        os.system('roslaunch wpb_home_bringup minimal.launch')
+        pass
     elif type == 'Stop':
         write_file('t\n')
     elif type == 'Begin Mapping':
