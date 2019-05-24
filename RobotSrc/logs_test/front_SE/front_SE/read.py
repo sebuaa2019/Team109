7cc2c8716809a7249428344941484e783dce2416
Liubowen <729270514@qq.com>
2019-05-24 21:49:30 +0800

    更改了views和models，删去小的测试部分

--- RobotSrc/logs_test/front_SE/front_SE/read.py
+++
@@ -1,6 +0,0 @@
-file = open('rosout.log')
-listx = []
-for line in file.readlines():
-    listx.append(line)
-
-print(listx)
--- RobotSrc/logs_test/front_SE/web/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class logs(models.Model):
-    time = models.CharField(max_length=30, default="")
-    type = models.CharField(max_length=10, default="")
-    info = models.CharField(max_length=100, default="")
