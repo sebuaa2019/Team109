dfc3eb245569eb1e1c0e06fab1b92413d8fd45dd
jy0205 <15652579915@163.com>
2019-05-27 12:03:59 +0800

    finish login and register

---
+++ RobotSrc/myweb/apps/users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class UserInfo(AbstractUser):
+    nick_name = models.CharField(max_length=20, verbose_name=u'昵称', default="")
+    Age = models.IntegerField(default=0,verbose_name=u'年龄')
+    gender = models.CharField(max_length=10, verbose_name=u'性别', choices=(('male', '男'), ('female', '女')),
+                              default="")
+    mobileNumber = models.CharField(max_length=11, null=True, blank=True, verbose_name=u'电话', default="")
+    address = models.CharField(max_length=100, null=True, blank=True, verbose_name=u'地址', default="")
+    userImage = models.ImageField(upload_to='image/%Y/%m', max_length=100,
+                                  verbose_name=u'头像')
+
+    class Meta:
+        verbose_name = '用户信息'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
